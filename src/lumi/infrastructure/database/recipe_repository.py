f91080a9509f62d50688818942218da3d871ad7f
Diogo Teodoro <diogo.teodoro015@gmail.com>
2026-03-17 11:26:37 -0300

    Refactor RecipeRepository to load recipes from JSON file and remove hardcoded recipes; add new recipes.json file

--- src/lumi/infrastructure/database/recipe_repository.py
+++ src/lumi/infrastructure/database/recipe_repository.py
@@ -1,49 +1,24 @@
 from lumi.domain.entities.recipe import Recipe
+import json
 
 class RecipeRepository:
     def __init__(self):
-        self._recipes = {
-    "panqueca": Recipe(
-        name="Panquecas",
-        ingredients=[
-            "1 xícara de farinha",
-            "2 colheres de sopa de açúcar",
-            "1 colher de sopa de fermento em pó",
-            "1 xícara de leite",
-            "1 ovo",
-            "2 colheres de sopa de manteiga derretida"
-        ],
-        description="Panqueca é um bolo achatado e redondo à base de massa, preparado em uma chapa ou frigideira quente, geralmente feito com farinha, leite, ovos e um agente de fermentação como fermento em pó.",
-        steps=[
-            "Em uma tigela, misture a farinha, o açúcar e o fermento em pó.",
-            "Em outra tigela, bata o leite, o ovo e a manteiga derretida.",
-            "Combine os ingredientes líquidos e secos até misturar levemente.",
-            "Aqueça uma frigideira antiaderente em fogo médio.",
-            "Despeje 1/4 de xícara de massa para cada panqueca.",
-            "Cozinhe até formar bolhas, depois vire e cozinhe até dourar."
-        ]
-    ),
+        self._recipes = {}
+        self.load_recipes_from_json('./lumi/infrastructure/database/recipes.json')
 
-    "arroz branco": Recipe(
-        name="Arroz Branco",
-        ingredients=[
-            "1 xícara de arroz branco",
-            "2 xícaras de água",
-            "1/2 colher de chá de sal",
-            "1 colher de sopa de manteiga (opcional)"
-        ],
-        description="O arroz branco é um grão básico, refinado e altamente versátil, processado pela remoção da casca, farelo e gérmen, restando apenas o endosperma branco rico em amido.",
-        steps=[
-            "Lave o arroz em água corrente fria até a água ficar transparente.",
-            "Em uma panela, leve a água para ferver.",
-            "Adicione o sal e a manteiga à água fervente.",
-            "Acrescente o arroz, reduza o fogo para baixo e tampe a panela.",
-            "Cozinhe por 18-20 minutos, ou até que a água seja absorvida e o arroz esteja macio.",
-            "Retire do fogo e deixe descansar, tampado, por 5 minutos. Solte com um garfo antes de servir."
-        ]
+    def load_recipes_from_json(self, file_path: str):
+        with open(file_path, 'r', encoding="utf-8") as file:
+            data = json.load(file)
 
-    )
-}
+            for key, item in data.items():
+                recipe = Recipe(
+                    name=item['name'],
+                    ingredients=item['ingredients'],
+                    description=item['description'],
+                    steps=item['steps']
+                )
+                self._recipes[key.lower()] = recipe
+
     def get_recipe_by_name(self, name: str) -> Recipe | None:
         print(name)
         recipe = self._recipes.get(name.lower())
